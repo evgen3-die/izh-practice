9819855d17b1279d274088b8a7e3638272bc8c73
Evgenii Kuznetsov <Evgenii Kuznetsov>
2016-08-31 18:07:34 +0500

    Добавлен начальный интерфейс

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,20 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    ChooseFile: TButton;
+    Label1: TLabel;
+    RGBLabel: TLabel;
+    SaveFile: TButton;
+    Image1: TImage;
+    Panel1: TPanel;
   private
     { private declarations }
   public
